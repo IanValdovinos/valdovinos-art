ebd3e1366d6b12bc3d7fa13894a87fc76f737300
Ian Samuel Valdovinos Granados <ian.valdovinos752001@gmail.com>
2025-10-29 18:37:04 -0400

    Add Hero Section

--- src/components/HeroSection/index.tsx
+++ src/components/HeroSection/index.tsx
@@ -3,9 +3,28 @@ import styles from "./HeroSection.module.css";
 
 function HeroSection() {
   return (
-    <div className={styles.heroSection}>
-      <div className={styles.imageContainer}></div>
-    </div>
+    <>
+      <div className={styles.heroSection}>
+        {/* Background Image */}
+        <div className={styles.imageContainer}>
+          <img src="/images/Valdovin University.jpg" alt="Hero Image" />
+        </div>
+
+        {/* Avatar */}
+        <div className={styles.avatarContainer}>
+          <img src="/images/profile_picture.jpg" alt="Avatar" />
+        </div>
+      </div>
+
+      <div className={styles.nameContainer}>
+        <h1 className={styles.nameContainerItem}>
+          David J. Valdovinos Granados
+        </h1>
+        <h2 className={styles.nameContainerItem}>
+          Architect | Traditional Artist | Art Teacher
+        </h2>
+      </div>
+    </>
   );
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,9 +4,6 @@ function Home() {
   return (
     <div>
       <HeroSection />
-      <h1>Home</h1>
-      <p>Welcome to David Valdovinos' Art Portfolio</p>
-      <p>Explore my artistic journey through the navigation above.</p>
     </div>
   );
 }
